0e59e3d2fbd83120cafcfc8cede9640cfd5c5df3
cananyagan11 <cananyagan11@gmail.com>
2022-10-31 11:10:32 +0300

    Add project files.

---
+++ Business/Conxrete/ProductManager.cs
@@ -0,0 +1,22 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Conxrete
+{
+    public class ProductManager : IProductManager
+    {
+        IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public List<Product> GetAll()
+        {
+            return _productDal.GetAll();
+        }
+    }
+}
