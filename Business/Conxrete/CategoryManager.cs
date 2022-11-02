7d34c0fc41f02be64c4b481f9d2c10e9d55180c3
cananyagan11 <cananyagan11@gmail.com>
2022-11-02 16:35:25 +0300

    DTO&Join

---
+++ Business/Conxrete/CategoryManager.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Conxrete
+{
+    public class CategoryManager : ICategoryManager
+    {
+        ICategoryDal _categorydal;
+
+        public CategoryManager(ICategoryDal categorydal)
+        {
+            _categorydal = categorydal;
+        }
+
+        public List<Category> GetAll()
+        {
+            //İş Kodları
+            return _categorydal.GetAll();
+        }
+
+        public Category GetById(int categoryId)
+        {
+            return _categorydal.Get(c=>c.CategoryId==categoryId);
+        }
+    }
+}
