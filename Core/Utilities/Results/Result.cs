ad87491836b6d280ca2ab8e7081f4523a6a73897
cananyagan11 <cananyagan11@gmail.com>
2022-11-03 10:03:43 +0300

    Create Results Class

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        public Result(bool success, string message):this(success)
+        {
+            Message = message;
+        }
+        //Override(2 cons. tek metot)
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        public bool Success { get;}
+
+        public string Message  {get;}
+    }
+}
